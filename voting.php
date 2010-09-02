47bd13b62c66453c3fa65d1aef6455e00570160b
_shade <timelessmomentum@gmail.com>
2010-09-01 23:44:22 -0700

    uggh

--- voting.php
+++ voting.php
@@ -1,6 +1,6 @@
 <?php
 
-/* This will be the voting file. That is, it handles voting songs up or down.
+/* This will be the voting file. That is, it handles requests for songs.
 
 When someone votes a song, it will store all the relevant information of that vote: time, ip, library, song.
 
@@ -12,14 +12,19 @@ Required:
 Returns:
     0           :vote counted
     1           :error, didn't receive proper arguments
+    2           :can't vote due to constraints
 */
 
 if (isset($_POST['lib']) && isset($_POST['song_id']))
 {
-    /* Add a positive vote to the song in the library */
+    /* Add a positive vote to the song in the library 
+       One check:
+            -one vote per song per ip
+    */
 
     require_once "connection.php";
 
+    $ip=$_SERVER['REMOTE_ADDR'];
 
 }
 else
