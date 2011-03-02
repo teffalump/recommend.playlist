262a95acee1a97dae687d49b62f934c679d7d8f5
teffalump <timelessmomentum@gmail.com>
2011-03-01 22:08:41 -0800

    fix: removed db insert, returns parsed playlist in array

--- parser.php
+++ parser.php
@@ -122,49 +122,6 @@ if (!empty($_FILES["playlist"]) && ($_FILES["playlist"]["error"] == 0 ) && isset
         unlink($_FILES["playlist"]["tmp_name"]);
         unset($_FILES["playlist"]);
       ####### END XSPF PARSER #########
-      /* ####### START DATABASE INSERT #######
-        * Put songs into right library
-        * --Check for: 
-        *       - song doesn't already exist
-        *           --> search for same id and lib_id
-        *
-        require_once "connection.php";
-        require_once "general.php";
-        require_once "variables.php";
-
-        $error_code=0;
-
-        $library_id = $_POST["library_id"];
-        $criteria = array("lib_id" => $library_id);
-        foreach ($track_array as $i)
-            {
-                $criteria["id"] = $song_info["id"];
-                $song_info = array(
-                        "metadata" => array(
-                            "title" => $i["title"], 
-                            "album" => $i["album"], 
-                            "artist" => $i["artist"], 
-                            "location" => $i["location"],
-                            "identifier" => $i["identifier"]
-                            ),
-                        "id" => substr(sha1($i["title"] . $i["album"] . $i["artist"]), 0, 10),
-                        "lib_id" => $library_id
-                        );
-                $obj = $db->SONG->findOne($criteria);
-                if (is_null($obj))
-                    {
-                        $db->SONG->save($song_info, array("safe" => TRUE));
-                        if (!is_null(get_value(lastError(), "err")))
-                            {
-                                $error_code = 1;
-                            }
-                    }
-            }
-        $connection->close();
-        ####### END DATABASE INSERT #######
-
-        echo $error_code;
-        exit;
-        */
+      return $track_array;
      }
 ?>
